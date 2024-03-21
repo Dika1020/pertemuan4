9d37bb249dfb5041ef8fc45436f8d9008b1519ef
Dika1020 <dikaputraganteng@gmail.com>
2024-03-21 20:54:27 +0700

    tugas 4

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; <a href="https://www.instagram.com/dikaa_oi/">Indika putra</a></span>
+        </div>
+    </div>
+</footer>
